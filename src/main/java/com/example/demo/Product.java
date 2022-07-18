3c88e3d7848d7321bb8b850c63678224c0b622c9
truonghusk17 <truonghusk17@gmail.com>
2022-07-19 06:04:40 +0700

    INTEGRATION: Repository Interface

--- src/main/java/com/example/demo/Product.java
+++ src/main/java/com/example/demo/Product.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 
 @Entity
 public class Product {
-	private int id;
+	private long id;
 	private String name;
 	private String brand;
 	private String madein;
@@ -18,11 +18,11 @@ public class Product {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public int getId() {
+	public long getId() {
 		return id;
 	}
 	
-	public void setId(int id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 	
